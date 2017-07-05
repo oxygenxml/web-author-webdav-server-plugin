e6f8bd10216cabf73d0e05eb8541d1526e831f29
Mihaela Calotescu <mihaela@oxygenxml.com>
2017-07-05 14:19:45 +0300

    WA-1377 Present samples in a Dashboard tab.

--- web/plugin.js
+++ web/plugin.js
@@ -1,5 +1,152 @@
 (function () {
 
+
+  /**
+   * A dashboard tab for files containers that have two actions: 'New' and 'Open'.
+   *
+   * @constructor
+   */
+  sync.ui.SamplesTab = function () {
+  };
+  goog.inherits(sync.ui.SamplesTab, sync.ui.AbstractDashboardTab);
+
+  /** @override */
+  sync.ui.SamplesTab.prototype.getTabHeaderImage = function () {
+    return '../static/images/Samples70.png';
+  };
+
+  /** @override */
+  sync.ui.SamplesTab.prototype.getTabHeaderDisplayName = function () {
+    return tr(msgs.SAMPLES_);
+  };
+
+  /** @override */
+  sync.ui.SamplesTab.prototype.renderTabContent = function (parentDiv) {
+    if (!this.samplesContainer) {
+      // icons sources
+      var images = [sync.util.computeHdpiIcon("/plugin-resources/webdav-server/gerbera.png"),
+        sync.util.computeHdpiIcon("plugin-resources/webdav-server/ditamap.png"),
+        sync.util.computeHdpiIcon("plugin-resources/webdav-server/docbook.png"),
+        sync.util.computeHdpiIcon("plugin-resources/webdav-server/xhtml.png"),
+        sync.util.computeHdpiIcon("plugin-resources/webdav-server/tei.png")];
+
+      // samples types.
+      var samples = ["DITA", "DITA Map", "DocBook", "XHTML", "TEI"];
+      // samples urls
+      var sampleUrls = ['dita/flowers/topics/flowers/gardenia.dita',
+        'dita/flowers/flowers.ditamap',
+        'docbook/v5/sample.xml',
+        'xhtml/sample.xml',
+        'tei/TEI-P5.xml'];
+      // samples ditamaps
+      var ditamaps = ['dita/flowers/flowers.ditamap', null, null, null, null];
+
+
+      // construct the dom structure for the samples.
+      var domHelper = new goog.dom.DomHelper();
+      this.samplesContainer = domHelper.createDom('div', {
+        id: 'dashboard-samples-container'
+      });
+      this.samplesContainer.style.display = 'none';
+      var titleCss = '';
+
+      for (var i = 0; i < samples.length; i++) {
+        var sampleLink = domHelper.createDom('a', 'dashboard-sample');
+        sampleLink.href = '#';
+        var sampleName = samples[i];
+        var sampleId = 'sample-title-' + sampleName.replace(' ', '-');
+        sampleLink.id = sampleId;
+
+        titleCss += '#' + sampleId + ':after{content:"' + sampleName + '";}\n';
+
+        var image = domHelper.createDom('img', 'dashboard-sample-image');
+        image.src = '../' + images[i];
+        sampleLink.appendChild(image);
+        this.samplesContainer.appendChild(sampleLink);
+
+        var author = sync.util.getURLParameter('author') || tr(msgs.ANONYMOUS_);
+
+        // open the sample document in a new tab when the sample image is clicked.
+        var openUrl = getUrl(sampleUrls[i], ditamaps[i], author);
+        goog.events.listen(image, goog.events.EventType.CLICK,
+          goog.bind(function (openUrl) {
+            window.open(openUrl)
+          }, this, openUrl));
+      }
+
+      // add styles before adding the elements.
+      addNewStylesheet(domHelper, titleCss);
+    }
+    parentDiv.appendChild(this.samplesContainer);
+  };
+
+  /**
+   * Compute the document url depending on params.
+   *
+   * @param docUrl the document url.
+   * @param ditamapUrl the ditamap url.
+   * @param authorName the author name.
+   *
+   * @return {string} the document url.
+   */
+  function getUrl(docUrl, ditamapUrl, authorName) {
+    var urlStr = "oxygen.html?";
+
+    urlStr += 'url=webdav-' + encodeURIComponent((webdavServerPluginUrl + docUrl).replace('\\', '/'));
+
+    urlStr += '&showSave=true';
+    if (ditamapUrl) {
+      urlStr += '&ditamap=webdav-' + encodeURIComponent((webdavServerPluginUrl + ditamapUrl).replace('\\', '/')) + '&';
+    }
+    urlStr += '&author=' + authorName;
+    return urlStr;
+  }
+
+  function addNewStylesheet(domHelper, dynamicCss) {
+    var cssFormating = domHelper.createDom('style');
+    cssFormating.innerHTML =
+      '#dashboard-samples-container.dashboard-section a{' +
+      'text-decoration:none;' +
+      '}' +
+
+      '#dashboard-samples-container {' +
+      'display: block !important;' +
+      'text-align: center;' +
+      '}' +
+
+      '#dashboard-samples-container .dashboard-sample-image {' +
+      'background-color: #FFFFFF;' +
+      'display: inline-block;' +
+      'height: 159px;' +
+      'width: 125px;' +
+      'margin:50px 30px 30px 30px;' +
+      '}' +
+
+      '.dashboard-sample {' +
+      'display: inline-block;margin: 10px;' +
+      '}' +
+
+      '.dashboard-sample:after {' +
+      'background-color: transparent;' +
+      'cursor:default;' +
+      'font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;' +
+      'color: #595959;' +
+      'display:block;' +
+      'position: relative;' +
+      '}' +
+      (dynamicCss ? dynamicCss : '') +
+
+      '#dashboard-container {' +
+      'box-sizing: border-box;' +
+      'border: 1px solid transparent;' +
+      'position: relative;' +
+      'min-height: 100%;' +
+      'padding-bottom: 50px;' +
+      '}';
+
+    document.head.appendChild(cssFormating);
+  }
+
   var translations = {
     "WEBDAV_READ_ONLY_MODE_": {
       "en_US":"The WebDAV server is in read-only mode. " +
@@ -30,6 +177,13 @@
       "fr_FR":"Anonyme",
       "ja_JP":"匿名",
       "nl_NL":"Anoniem"
+    },
+    "SAMPLES_": {
+      "en_US":"Samples",
+      "de_DE":"Beispiele",
+      "fr_FR":"Exemples",
+      "ja_JP":"例",
+      "nl_NL":"Voorbeelden"
     }
   };
   sync.Translation.addTranslations(translations);
@@ -51,9 +205,9 @@
   }
   // load samples thumbails.
   goog.events.listen(
-    workspace, sync.api.Workspace.EventType.DASHBOARD_LOADED, function() {
+    workspace, sync.api.Workspace.EventType.BEFORE_DASHBOARD_LOADED, function() {
       if (sync.options.PluginsOptions.getClientOption('display_samples') == "on") {
-        displayWebdavSamples();
+        workspace.addDashboardAdditionalTabs(new sync.ui.SamplesTab());
       }
     });
 
@@ -109,138 +263,6 @@
         });
       }
     });
-  }
-
-  /**
-   * Display the webdav samples on the dashboard.
-   */
-  function displayWebdavSamples() {
-    // icons sources
-    var images = [sync.util.computeHdpiIcon("/plugin-resources/webdav-server/gerbera.png"),
-      sync.util.computeHdpiIcon("plugin-resources/webdav-server/ditamap.png"),
-      sync.util.computeHdpiIcon("plugin-resources/webdav-server/docbook.png"),
-      sync.util.computeHdpiIcon("plugin-resources/webdav-server/xhtml.png"),
-      sync.util.computeHdpiIcon("plugin-resources/webdav-server/tei.png")];
-
-    // samples types.
-    var samples = ["DITA", "DITA Map", "DocBook", "XHTML", "TEI"];
-    // samples urls
-    var sampleUrls = ['dita/flowers/topics/flowers/gardenia.dita',
-      'dita/flowers/flowers.ditamap',
-      'docbook/v5/sample.xml',
-      'xhtml/sample.xml',
-      'tei/TEI-P5.xml'];
-    // samples ditamaps
-    var ditamaps = ['dita/flowers/flowers.ditamap', null, null, null, null];
-
-    // construct the dom structure for the samples.
-    var domHelper = new goog.dom.DomHelper();
-    var samplesContainer = domHelper.createDom('div', 'dashboard-section');
-    samplesContainer.id = 'samples-container';
-    samplesContainer.style.display = 'none';
-    var titleCss = '';
-
-    for (var i = 0; i < samples.length; i++) {
-      var sampleLink = domHelper.createDom('a', 'dashboard-sample');
-      sampleLink.href = '#';
-      var sampleName = samples[i];
-      var sampleId = 'sample-title-' + sampleName.replace(' ', '-');
-      sampleLink.id = sampleId;
-
-      titleCss += '#' + sampleId + ':after{content:"' + sampleName + '";}\n';
-
-      var image = domHelper.createDom('img', 'dashboard-sample-image');
-      image.src = '../' + images[i];
-      sampleLink.appendChild(image);
-      samplesContainer.appendChild(sampleLink);
-
-      var author = sync.util.getURLParameter('author') || tr(msgs.ANONYMOUS_);
-
-      // open the sample document in a new tab when the sample image is clicked.
-      var openUrl = getUrl(sampleUrls[i], ditamaps[i], author);
-      goog.events.listen(image, goog.events.EventType.CLICK,
-        goog.bind(function (openUrl) {
-          window.open(openUrl)
-        }, this, openUrl));
-    }
-
-    // add styles before adding the elements.
-    addNewStylesheet(domHelper, titleCss);
-
-    // the dashboard containing element.
-    var container = document.getElementById('dashboard-container');
-    container.appendChild(samplesContainer);
-  }
-
-  /**
-   * Compute the document url depending on params.
-   *
-   * @param docUrl the document url.
-   * @param ditamapUrl the ditamap url.
-   * @param authorName the author name.
-   *
-   * @return {string} the document url.
-   */
-  function getUrl(docUrl, ditamapUrl, authorName) {
-    var urlStr = "oxygen.html?";
-
-    urlStr += 'url=webdav-' + encodeURIComponent((webdavServerPluginUrl + docUrl).replace('\\', '/'));
-
-    urlStr += '&showSave=true';
-    if (ditamapUrl) {
-      urlStr += '&ditamap=webdav-' + encodeURIComponent((webdavServerPluginUrl + ditamapUrl).replace('\\', '/')) + '&';
-    }
-    urlStr += '&author=' + authorName;
-    return urlStr;
-  }
-
-  function addNewStylesheet(domHelper, dynamicCss) {
-    var cssFormating = domHelper.createDom('style');
-    cssFormating.innerHTML =
-      '#samples-container.dashboard-section a{' +
-      'text-decoration:none;' +
-      '}' +
-
-      '#samples-container:before{' +
-      'content:"Samples";' +
-      '}' +
-
-      '#samples-container {' +
-      'display: block !important;' +
-      'text-align: center;' +
-      '}' +
-
-      '#samples-container .dashboard-sample-image {' +
-      'background-color: #FFFFFF;' +
-      'display: inline-block;' +
-      'height: 159px;' +
-      'width: 125px;' +
-      'margin:10px;' +
-      '}' +
-
-      '.dashboard-sample {' +
-      'display: inline-block;margin: 10px;' +
-      '}' +
-
-      '.dashboard-sample:after {' +
-      'background-color: transparent;' +
-      'cursor:default;' +
-      'font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;' +
-      'color: #595959;' +
-      'display:block;' +
-      'position: relative;' +
-      '}' +
-      (dynamicCss ? dynamicCss : '') +
-
-      '#dashboard-container {' +
-      'box-sizing: border-box;' +
-      'border: 1px solid transparent;' +
-      'position: relative;' +
-      'min-height: 100%;' +
-      'padding-bottom: 50px;' +
-      '}';
-
-    document.head.appendChild(cssFormating);
   }
 
   function loadEditorCSS() {
