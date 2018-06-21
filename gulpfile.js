5734b5d24c00361694a85a564d4dda5a17d723b9
andreip1123 <andrei_popa@sync.ro>
2018-06-21 11:36:02 +0300

    WA-1233: Use translation module, add uglify step in build

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var Synci18n = require('sync-i18n');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var webLocation = 'web';
+var targetLocation = "target/web";
+
+/**
+ * Concatenate all js files into one, then uglify it.
+ */
+gulp.task('prepare-package', ['i18n'], function() {
+  return gulp.src(webLocation + '/*.js')
+    .pipe(concat('plugin.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(targetLocation));
+});
+
+gulp.task('i18n', function () {
+  Synci18n().generateTranslations();
+});
+
+gulp.task('default', ['prepare-package']);
