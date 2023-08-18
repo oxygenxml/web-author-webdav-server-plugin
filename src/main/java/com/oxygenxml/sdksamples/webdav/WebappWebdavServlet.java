da6a68cb0705dcf404e512f226fd0bc93a765376
Dumitru Bogdan <bogdan_dumitru@sync.ro>
2023-08-18 16:35:04 +0300

    WA-6683: Implement CSRF security.
    nu am putut sa trec la ServletPluginExtension ca sa beneficiez automat
    de securitatea built-in de CSRF, dar am implementat-o in plugin.

--- src/main/java/com/oxygenxml/sdksamples/webdav/WebappWebdavServlet.java
+++ src/main/java/com/oxygenxml/sdksamples/webdav/WebappWebdavServlet.java
@@ -6,11 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import ro.sync.ecss.extensions.api.webapp.plugin.WebappServletPluginExtension;
 
 /**
  * The wrapper class for the tomcat webdav servlet.
  */
+@Slf4j
 public class WebappWebdavServlet extends WebappServletPluginExtension {
   
   public static final String WEBDAV_SERVER = "webdav-server";
@@ -25,10 +27,20 @@ public class WebappWebdavServlet extends WebappServletPluginExtension {
     webdavWrapper.init(this.config);
   }
 
-
   @Override
   public void service(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    String method = req.getMethod();
+    if (req.getHeader("X-Requested-With") == null
+        && !"GET".equals(method)
+        && !"PROPFIND".equals(method)) {
+      String requestPath = req.getServletPath() + (req.getPathInfo() != null ? req.getPathInfo() : "");
+      log.error(method + " request denied by CSRF security to " + requestPath
+          + ". For more information, see: https://www.oxygenxml.com/doc/ug-waCustom/topics/wa-csrf.html");
+      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+   
     webdavWrapper.service(req, resp);
   }
   
